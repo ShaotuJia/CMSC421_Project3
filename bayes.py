48ef8000151a1754eaaf815ecbc58e98f685f77c
ShaotuJia <shaotu.jia@hotmail.com>
2017-07-08 05:11:07 -0400

    add bayes.py

---
+++ bayes.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+# Brief: This project is to estimate probability that each record in classify.data is a democrat representative
+# Assumption: at first, assume that each field is independent to others
+# Step 1: Load train data Set
+# Step 2: Find the relation between party (democrat) and vote (Y, N, ?) in each field
+# Step 3: Using obtained relation to estimate in classify.data
